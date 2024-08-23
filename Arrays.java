a64af65bdbbedec5c6b9060ff857361c1a567b8d
Shubhraj Prasad Singh <ashu.shubhraj@gmail.com>
2024-08-24 00:16:46 +0530

    Index of N by BS

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Arrays {
 
     public static void main(String[] args) {
--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,4 @@
-public class Main {
+public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {5, 6, 7, 8, 1, 2, 3, 4};
         int key = 1;
@@ -6,7 +6,43 @@ public class Main {
         System.out.println("Index of " + key + " is: " + index);
         
     }
-public static int findElementInRotatedArray(int[] arr, int key) {
+
+    /**
+     * Find the start and end index of N in a sorted array
+     * We will run two BS one for first index and one for end index
+     */
+
+    public static void firstAndLastIndex(int[] arr, int key){
+        int left = 0, right = arr.length-1;
+        int mid = (left + right) / 2;
+        int firstIndex = -1, lastIndex = -1;
+        while(left <= right){
+            if(arr[mid] == key){
+                firstIndex = mid;
+                right = mid - 1;
+            } else if(arr[mid]<key){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        left = 0;
+        right = arr.length - 1;
+        while(left <= right){
+            if(arr[mid] == key){
+                lastIndex = mid;
+                left = mid + 1;
+            } else if(arr[mid]<key){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("First index of " + key + " is: " + firstIndex);
+        System.out.println("Last index of " + key + " is: " + lastIndex);
+    }
+
+    public static int findElementInRotatedArray(int[] arr, int key) {
         int left = 0;
         int right = arr.length - 1;
 
@@ -42,3 +78,4 @@ public static int findElementInRotatedArray(int[] arr, int key) {
         return -1;
     }
 }
+    
