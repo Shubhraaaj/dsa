65d3bbb7b7f8c3ced0c7e57840ff183c0249cc1b
Shubhraj Prasad Singh <ashu.shubhraj@gmail.com>
2024-08-24 00:48:38 +0530

    Search element in 2D sorted array

--- BinarySearch.java
+++ BinarySearch.java
@@ -77,5 +77,83 @@ public class BinarySearch {
         // If the element is not found
         return -1;
     }
+
+    /**
+     * Find minimum element in a sorted and rotated array
+     */
+
+     public static int findMin(int[] arr) {
+        int left = 0, right = arr.length -1;
+        while(left < right){
+            int mid = (left + right) / 2;
+            if(arr[mid]<arr[right]){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return arr[left];
+     }
+
+    /**
+     * Find the index of the first element in a sorted and rotated array
+     * 
+     * We need to find the index of the smallest element in a sorted and rotated array
+     * Then Binary search in two parts from 0 to minIndex and minIndex to arr.length -1
+     */
+    public static int binarySearch(int[] arr, int left, int right, int target){
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(arr[mid] == target){
+                return mid;
+            } else if(arr[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int findMinIndex(int[] arr){
+        int left = 0, right = arr.length - 1;
+        while(left < right){
+            int mid = (left + right) / 2;
+            if(arr[mid] < arr[right]){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static int findIndexSortedRotated(int[] arr, int key){
+        int minIdx = findMinIndex(arr);
+        int ans = binarySearch(arr, 0, minIdx -1, key);
+        if(ans == -1)
+            ans = binarySearch(arr, minIdx -1, arr.length - 1, key);
+        return ans;
+    }
+
+    /**
+     * Check if the element is present in a sorted 2D array
+     * Start from top right corner and then start traversing
+     * If the element is greater than arr(i,j) then go to the next row else go to the previous column
+     */
+
+     public static boolean findElementIn2DArray(int[][] arr, int target){
+        int i = 0, j = arr.length - 1;
+        while(i< arr.length && j>=0){
+            if(arr[i][j]==target){
+                return true;
+            } else if(arr[i][j]<target){
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+     }
 }
     
